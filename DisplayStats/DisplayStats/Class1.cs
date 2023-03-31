ac04ee4f6ad8a13da6a414e068e8ee73229bc560
Oskar Smedenmark <oskar.smedenmark@gmail.com>
2023-03-31 11:31:07 +0200

    Basic Mod Setup with templates

--- DisplayStats/DisplayStats/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DisplayStats
-{
-    public class Class1
-    {
-    }
-}
---
+++ DisplayStats/DisplayStats/DisplayStats.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using DisplayStats.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+
+namespace DisplayStats
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class DisplayStats : BaseUnityPlugin
+    {
+        private const string ModId = "com.zirhax.rounds.DisplayStats";
+        private const string ModName = "DisplayStats";
+        public const string Version = "1.0.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "DS";
+
+        public static DisplayStats Instance { get; private set; }
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            CustomCard.BuildCard<MyCardName>();
+            Instance = this;
+        }
+    }
+}
